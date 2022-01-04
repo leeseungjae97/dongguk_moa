d592b7dff399b4aff0a31f141c876e87e508ebef
leeseungjae97 <61075204+leeseungjae97@users.noreply.github.com>
2022-01-04 23:18:00 +0900

    Selenium으로 로그인 진행
    #document 관련 가상요소로 인해 eclass는 jsoup 사용힘듦

--- src/main/java/moa/MainController.java
+++ src/main/java/moa/MainController.java
@@ -1,24 +1,55 @@
 package moa;
 
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import javafx.fxml.FXML;
 import moa.arguments.StringArgu;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class MainController {
     @FXML
     void initialize() {
-        getEclassPage();
+        System.setProperty("webdriver.chrome.driver", "E:\\chromedriver_win32\\chromedriver.exe");
+        getSmartPage();
+        //getEclassPage();
+    }
+    public void getSmartPage() {
+        ChromeOptions chromeHeadlessOption = new ChromeOptions();
+        chromeHeadlessOption.addArguments("headless");
+        WebDriver smartAtDriver = new ChromeDriver(chromeHeadlessOption);
+        smartAtDriver.get(StringArgu.SMART_ATTENDANCE);
+
+        WebElement smartLoginBtn = new WebDriverWait(smartAtDriver, Duration.ofSeconds(2))
+                .until(ExpectedConditions.elementToBeClickable(By.id("login")));
+
+        WebElement smartId = smartAtDriver.findElement(By.id("loginId"));
+        WebElement smartPassword = smartAtDriver.findElement(By.id("password"));
+//        WebElement smartLoginBtn = smartAtDriver.findElement(By.linkText("로그인"));
+        
+        smartId.sendKeys("2016210162");
+        smartPassword.sendKeys("Asqw!2qwas");
+
+        smartLoginBtn.click();
+
+        new WebDriverWait(smartAtDriver, Duration.ofSeconds(2))
+                .until(ExpectedConditions.elementToBeClickable(By.id("view_today")));
+
+        WebElement subjectTable = smartAtDriver.findElement(By.id("lecture"));
+        List<WebElement> clickableSubjects = subjectTable.findElements(By.cssSelector("#lecture > tbody > tr"));
+
+        clickableSubjects.get(0).click();
+
     }
     public void getEclassPage() {
-        try {
-            WebClient webClient = new WebClient();
-            HtmlPage page = webClient.getPage(StringArgu.ECLASS);
-            HtmlUnit
-            System.out.println(page.getDocumentElement());
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+// Jsoup 방식 --
+
 //        HttpGet request = new HttpGet(StringArgu.ECLASS);
 //
 //        Document doc = null;
@@ -39,5 +70,40 @@ public class MainController {
 //        } catch (IOException e) {
 //            System.out.println(e.getMessage());
 //        }
+
+// Selenium 방식 --
+        ChromeOptions chromeHeadlessOption = new ChromeOptions();
+        chromeHeadlessOption.addArguments("headless");
+
+        WebDriver eClassDriver = new ChromeDriver(chromeHeadlessOption);
+        eClassDriver.get(StringArgu.ECLASS);
+
+        eClassDriver.switchTo().frame("main");
+        //학생/교직원 로그인 버튼
+        WebElement openLoginInput = eClassDriver.findElement(By.linkText("학생/교직원 로그인"));
+        openLoginInput.click();
+
+        //아이디, 패스워드 입력창
+        WebElement eclassId = eClassDriver.findElement(By.name("userDTO.userId"));
+        WebElement eclassPassword = eClassDriver.findElement(By.name("userDTO.password"));
+        //로그인 버튼
+        WebElement loginBtn = eClassDriver.findElement(By.className("loginBtn"));
+
+        //로그인
+        eclassId.sendKeys("2016210162");
+        eclassPassword.sendKeys("Asqw!2qwas");
+
+        loginBtn.click();
+
+        //로그인 후 창 리프레시되는거 기다리기. (drop down 되는 span을 기다림)
+        new WebDriverWait(eClassDriver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.elementToBeClickable(By.tagName("b")));
+
+        //내 강의실 목록의 버튼들 가져오기
+        WebElement clickSubject = eClassDriver.findElement(By.id("mCSB_1"));
+        List<WebElement> clickableSubjects = clickSubject.findElements(By.tagName("button"));
+
+        //첫번째 강의실 클릭.
+        clickableSubjects.get(0).click();
     }
 }
